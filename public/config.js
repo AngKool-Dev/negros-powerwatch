00636e22e899a3d58e7ee7e658a3782573b99ab2
Era <era@users.noreply.github.com>
2026-09-05 11:42:02 +0800

    Point frontend to deployed Cloudflare Worker API

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
 window.POWERWATCH_CONFIG = window.POWERWATCH_CONFIG || {};
-window.POWERWATCH_CONFIG.API_BASE = window.POWERWATCH_CONFIG.API_BASE || '/api/v1';
+window.POWERWATCH_CONFIG.API_BASE = window.POWERWATCH_CONFIG.API_BASE || 'https://negros-powerwatch-worker.asoniojohnpaul.workers.dev/api/v1';
